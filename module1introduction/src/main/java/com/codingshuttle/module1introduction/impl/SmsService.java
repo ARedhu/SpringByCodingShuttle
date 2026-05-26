1c56b9952959f37faffa9f56fed7b2cbfba741cf
Ashish Redhu <redhu5716@gmail.com>
2026-05-26 16:01:26 +0530

    ConditionalOnProperty & Fetch all implementation of Interface and beans Done

--- module1introduction/src/main/java/com/codingshuttle/module1introduction/impl/SmsService.java
+++ module1introduction/src/main/java/com/codingshuttle/module1introduction/impl/SmsService.java
@@ -2,10 +2,13 @@ package com.codingshuttle.module1introduction.impl;
 
 import com.codingshuttle.module1introduction.NotificationService;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 
 @Component
-@Qualifier("smsNotif")
+//@Qualifier("smsNotif")
+//@ConditionalOnProperty(name = "notification.type", havingValue = "smsNotif")
+
 public class SmsService implements NotificationService {
     @Override
     public void send(String msg){
