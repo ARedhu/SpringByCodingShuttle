c72ab31b8344fec213d4d1549f0b2aa28dbe6a80
ARedhu <redhu5716@gmail.com>
2026-07-21 17:49:30 +0530

    Logging in Spring Boot done

--- prod-ready-features/src/main/java/com/codingShuttle/Ashish/prod_ready_features/clients/impl/EmployeeClientImpl.java
+++ prod-ready-features/src/main/java/com/codingShuttle/Ashish/prod_ready_features/clients/impl/EmployeeClientImpl.java
@@ -5,6 +5,8 @@ import com.codingShuttle.Ashish.prod_ready_features.clients.EmployeeClient;
 import com.codingShuttle.Ashish.prod_ready_features.dto.EmployeeDTO;
 import com.codingShuttle.Ashish.prod_ready_features.exceptions.ResourceNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
@@ -18,23 +20,40 @@ public class EmployeeClientImpl implements EmployeeClient {
 
     private final RestClient restClient;
 
+    Logger log = LoggerFactory.getLogger(EmployeeClientImpl.class); // Here it is necessary to specify the name of the class for which we are creating this logger. So, that the logger can log the name of the class properly while logging in the file/console.
+
     @Override
     public List<EmployeeDTO> getAllEmployees() {
+
+        // Remember the below logs have no effect, these are just the marker for the understanding of logs by the fellow programmers.
+        // log.error("error log");
+        // log.warn("warn log");
+        // log.info("info log");
+        // the below two logs will not get printed as bydefault only till "INFO" logs are enabled for root. We can change it in "application.properties" file.
+        // log.debug("debug log");
+        // log.trace("tracel log");
+
+        // Write only trace here. Don't write "INFO"
+        log.trace("Trying to retrieve all employees in getAllEmployees");
         try{
             ApiResponse<List<EmployeeDTO>> employeeDTOList = restClient.get()
                                                         .uri("employee")// This thing append in the base url.
                                                         .retrieve()
                                                         .body(new ParameterizedTypeReference<>(){}); // It tells the type of response we want. If we have a single employee then we could have easily written like "EmployeeDTO.class". But as we have a parameterized list here. So, we used this.
             // There can occur various errors in the upper mentioned code lines like client, server, connection errors.
+            log.debug("Successfully retrived the employees in getAllEmployees");
+            log.trace("Retrieved employees list in getAllEmployees: {}, {}", employeeDTOList.getData(), "done"); // It is better to use "{}" as compared to string concatination as we can directly pass multiple variables data in a more organized way.
             return employeeDTOList.getData();
         }
         catch(Exception e){
+            log.error("Exception occured in getAllEmployes: ", e);
             throw new RuntimeException(e);
         }
     }
 
     @Override
     public EmployeeDTO getEmployeeById(Long employeeId) {
+        log.trace("Trying to get Employee by id in getEmployeeById with id: {}", employeeId);
         try{
             ApiResponse<EmployeeDTO> employeeDTOApiResponse = restClient.get()
                                                                     .uri("employee/{employeeId}", employeeId) //  We can pass multiple other variables as well like: .uri("employee/{employeeId}/{abc}", employeeId, "hello")
@@ -55,6 +74,8 @@ public class EmployeeClientImpl implements EmployeeClient {
                     .body(employeeDTO)
                     .retrieve()
                     .onStatus(HttpStatusCode::is4xxClientError, (req, res)->{ // In this way we can handle the exceptions.
+                        log.debug("4xxClient error occurred during createNewEmployee");
+                        log.error(new String(res.getBody().readAllBytes()));
                         throw new ResourceNotFoundException("could not create the employee");
                     })
                     .body(new ParameterizedTypeReference<>(){});
--- prod-ready-features/src/main/java/com/codingShuttle/Ashish/prod_ready_features/notes/Notes.java
+++ prod-ready-features/src/main/java/com/codingShuttle/Ashish/prod_ready_features/notes/Notes.java
@@ -31,3 +31,42 @@ Note: We can use both JPA auditing and envers together.
 Question: What will happen insideo of DB?
 Ans: It will create one extra table for all i.e. revinfo. And, it will create one more table for each @Audited table by enver and that is like: post_AUD. But remember the database will fill very fast.
  */
+
+
+
+/*
+
+Logger: Captures the message
+Formatter: Formats the messages captured by logger.
+Handler: Dispatches the messages by printing them on the console, or storing them in a file, sending an email etc.
+
+* Log Levels:
+- FATAL: fatal error crashing the system
+- ERROR: runtime errors
+- WARN: warning, should work on it, can create errors later on.
+- INFO: events occuring at the run time. By default root level logging is set to INFO.
+- DEBUG: information about the flow of the system of programmers.
+- TRACE: more detailed info about the flow of the system.
+
+
+Note:
+If we set logging of a file to a particular level let's say "INFO", then it will log only till INFO(FATAL, ERROR, WARN, INFO) and not log (DEBUG and TRACE).
+
+Log Formatters
+The log messages can be formatted and customized according to our requirements.
+
+logging.pattern.console= %d [%level] %c{1.} [%t] %m%n
+- %d - date
+- %level - log level
+- %c - class path
+- %t - thread executing
+- %m - message
+- %n - new line
+
+%c{2.} -> now it will show the log from current file and parent file, means till 2-levels.
+
+
+ */
+
+
+
